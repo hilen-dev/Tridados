e7962c67bfd8ff31619b168cef81433edd917cb5
hilen-dev <joneshilen@gmail.com>
2025-11-13 21:58:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # Página inicial genérica
+    return render_template('index.html')
+
+@app.route('/criar_ficha', methods=['GET', 'POST'])
+def criar_ficha():
+    if request.method == 'POST':
+        # Aqui depois vamos salvar no banco ou em arquivo
+        nome = request.form['nome']
+        idade = request.form['idade']
+        pericias = request.form['pericias']
+        poder = request.form['poder']
+        habilidade = request.form['habilidade']
+        resistencia = request.form['resistencia']
+        vantagens = request.form['vantagens']
+        historico = request.form['historico']
+
+        print("FICHA RECEBIDA:", nome)  # só pra testar
+
+    return render_template('criar_ficha.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
