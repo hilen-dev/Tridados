73fac46ee0dbe8a51ec9b23a61901d53207295b8
hilen-dev <joneshilen@gmail.com>
2026-01-27 10:19:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session["ficha"]["arquetipo"] = request.form.get("arquetipo")
 import os
 import json
 import uuid
